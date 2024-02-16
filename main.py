e1b0040c867f9c4de78fe7f01aa71f6923dc7fe7
crazytct <156101760+crazytct@users.noreply.github.com>
2024-02-16 20:10:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ import aiohttp
 import base64
 import time
 from io import BytesIO
-from utils.Tools import * 
-from utils.config import *
+
 
 os.environ["JISHAKU_NO_DM_TRACEBACK"] = "False"
 os.environ["JISHAKU_HIDE"] = "True"
